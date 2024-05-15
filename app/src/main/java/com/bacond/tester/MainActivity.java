eb9ffe6e3e9e95d746035ce632eb83e24b65b29b
Gunther <abraham.lenson@mailbox.org>
2024-05-15 19:15:47 +0700

    First Commit

---
+++ app/src/main/java/com/bacond/tester/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bacond.tester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.keyboard_view);
+    }
+}
